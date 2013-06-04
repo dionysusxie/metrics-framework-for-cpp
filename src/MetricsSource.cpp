f679c28f669190211adbd112f4002a846774be89
Dio Xie <xieliangjxnu@qq.com>
2013-06-04 17:10:29 +0800

    A new class: MetricsSource

---
+++ src/MetricsSource.cpp
@@ -0,0 +1,21 @@
+/*
+ * MetricsSource.cpp
+ *
+ *  Created on: Jun 4, 2013
+ *      Author: xieliang
+ */
+
+#include "MetricsSource.h"
+
+namespace gmf {
+
+MetricsSource::MetricsSource() {
+    // TODO Auto-generated constructor stub
+
+}
+
+MetricsSource::~MetricsSource() {
+    // TODO Auto-generated destructor stub
+}
+
+} /* namespace gmf */
---
+++ src/MetricsSource.h
@@ -0,0 +1,20 @@
+/*
+ * MetricsSource.h
+ *
+ *  Created on: Jun 4, 2013
+ *      Author: xieliang
+ */
+
+#ifndef METRICSSOURCE_H_
+#define METRICSSOURCE_H_
+
+namespace gmf {
+
+class MetricsSource {
+public:
+    MetricsSource();
+    virtual ~MetricsSource();
+};
+
+} /* namespace gmf */
+#endif /* METRICSSOURCE_H_ */
