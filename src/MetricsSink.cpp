38f44820f06887da38cd0ad66852cc5cde99b0b1
Dio Xie <xieliangjxnu@qq.com>
2013-06-04 17:12:27 +0800

    A new class: MetricsSink

---
+++ src/MetricsSink.cpp
@@ -0,0 +1,21 @@
+/*
+ * MetricsSink.cpp
+ *
+ *  Created on: Jun 4, 2013
+ *      Author: xieliang
+ */
+
+#include "MetricsSink.h"
+
+namespace gmf {
+
+MetricsSink::MetricsSink() {
+    // TODO Auto-generated constructor stub
+
+}
+
+MetricsSink::~MetricsSink() {
+    // TODO Auto-generated destructor stub
+}
+
+} /* namespace gmf */
---
+++ src/MetricsSink.h
@@ -0,0 +1,20 @@
+/*
+ * MetricsSink.h
+ *
+ *  Created on: Jun 4, 2013
+ *      Author: xieliang
+ */
+
+#ifndef METRICSSINK_H_
+#define METRICSSINK_H_
+
+namespace gmf {
+
+class MetricsSink {
+public:
+    MetricsSink();
+    virtual ~MetricsSink();
+};
+
+} /* namespace gmf */
+#endif /* METRICSSINK_H_ */
