deb7cecac2185cc19776bcb56753456fe18e218c
Dio Xie <xieliangjxnu@qq.com>
2013-06-20 18:05:03 +0800

    New class: MutableMetric

---
+++ src/MutableMetric.cpp
@@ -0,0 +1,21 @@
+/*
+ * MutableMetric.cpp
+ *
+ *  Created on: Jun 20, 2013
+ *      Author: xieliang
+ */
+
+#include "MutableMetric.h"
+
+namespace gmf {
+
+MutableMetric::MutableMetric() {
+    // TODO Auto-generated constructor stub
+
+}
+
+MutableMetric::~MutableMetric() {
+    // TODO Auto-generated destructor stub
+}
+
+} /* namespace gmf */
---
+++ src/MutableMetric.h
@@ -0,0 +1,20 @@
+/*
+ * MutableMetric.h
+ *
+ *  Created on: Jun 20, 2013
+ *      Author: xieliang
+ */
+
+#ifndef MUTABLEMETRIC_H_
+#define MUTABLEMETRIC_H_
+
+namespace gmf {
+
+class MutableMetric {
+public:
+    MutableMetric();
+    virtual ~MutableMetric();
+};
+
+} /* namespace gmf */
+#endif /* MUTABLEMETRIC_H_ */
