cabf45408256171dac2ea465337393e97f67ab52
Dio Xie <xieliangjxnu@qq.com>
2013-06-24 14:07:31 +0800

    Add some methods in class MutableMetric

--- src/MutableMetric.cpp
+++ src/MutableMetric.cpp
@@ -9,13 +9,30 @@
 
 namespace gmf {
 
-MutableMetric::MutableMetric() {
-    // TODO Auto-generated constructor stub
-
+MutableMetric::MutableMetric():
+        changed_(true) {
 }
 
 MutableMetric::~MutableMetric() {
-    // TODO Auto-generated destructor stub
+}
+
+void MutableMetric::snapshot(MetricsRecordBuilder& builder) {
+    this->snapshot(builder, false);
+}
+
+bool MutableMetric::changed() {
+    boost::shared_lock<boost::shared_mutex> read_lock(this->changed_mutex_);
+    return this->changed_;
+}
+
+void MutableMetric::setChanged() {
+    boost::lock_guard<boost::shared_mutex> write_lock(this->changed_mutex_);
+    this->changed_ = true;
+}
+
+void MutableMetric::clearChanged() {
+    boost::lock_guard<boost::shared_mutex> write_lock(this->changed_mutex_);
+    this->changed_ = false;
 }
 
 } /* namespace gmf */
--- src/MutableMetric.h
+++ src/MutableMetric.h
@@ -8,12 +8,27 @@
 #ifndef MUTABLEMETRIC_H_
 #define MUTABLEMETRIC_H_
 
+#include <boost/thread.hpp>
+#include "MetricsRecord.h"
+
+
 namespace gmf {
 
 class MutableMetric {
 public:
     MutableMetric();
     virtual ~MutableMetric();
+public:
+    // all: if true, snapshot unchanged metrics as well
+    virtual void snapshot(MetricsRecordBuilder& builder, bool all) = 0;
+    void snapshot(MetricsRecordBuilder& builder);
+    bool changed();
+protected:
+    void setChanged();
+    void clearChanged();
+private:
+    bool changed_;
+    boost::shared_mutex changed_mutex_;
 };
 
 } /* namespace gmf */
