320cd76f2c02a222ee686b55c76744071b949391
Dio Xie <xieliangjxnu@qq.com>
2013-06-27 18:44:17 +0800

    Add test for Float & Double

--- src/Number_unittest.cpp
+++ src/Number_unittest.cpp
@@ -6,14 +6,44 @@
  */
 
 
-
+#include <cmath>
 #include <gtest/gtest.h>
 #include "Number.h"
 
 using namespace gmf;
 using namespace gmf::number;
+using namespace std;
+
+template<class T>
+bool float_equal(T a, T b) {
+    const T small = 0.00001;
+    return std::abs(a - b) < small;
+}
 
 TEST(NumberTest, Int) {
     Int a(1);
-    EXPECT_EQ(1, a.intValue());
+    EXPECT_EQ(1, a.value());
+    EXPECT_EQ(INT, a.type());
+}
+
+TEST(NumberTest, Long) {
+    Long a(1);
+    EXPECT_EQ(1, a.value());
+    EXPECT_EQ(LONG, a.type());
+}
+
+TEST(NumberTest, Float) {
+    const float num = 1.0002;
+    Float a(num);
+
+    EXPECT_TRUE(float_equal(num, a.value()));
+    EXPECT_EQ(FLOAT, a.type());
+}
+
+TEST(NumberTest, Double) {
+    const double num = 1.0002;
+    Double a(num);
+
+    EXPECT_TRUE(float_equal(num, a.value()));
+    EXPECT_EQ(DOUBLE, a.type());
 }
